54350f555eaf32b29b5a20079c38cd837d43dffd
Vlad <ForFriApple@gmail.com>
2017-06-14 12:14:57 +0300

    add some stuff

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 $(window).scroll(function() {
-    if ($(document).scrollTop() > 100){
+    if ($(document).scrollTop() > 110){
         $('nav').addClass('nav-shrink');
     } else {
         $('nav').removeClass('nav-shrink');
     }
+});
+
+$(document).ready(function(){
+    $(".slow-scroll").on("click", "a", function(event){
+        event.preventDefault();
+        var id = $(this).attr("href"),
+        top = $(id).offset().top;
+        $("body,html").animate({scrollTop: top-30}, 1500);
+    });
+});
+
+
+$(document).ready(function(){
+    $(".works-btn").on("click", "i", function(){
+        var pos = $(this).offset().top;
+        $("body,html").animate({scrollTop: pos+50}, 1200);
+    });
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip({
+      delay: { show: 250, hide: 200 }
+  });
 });
